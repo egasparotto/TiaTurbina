900c877ec49d4ad489f828f9adbcf447061a70e8
Eduardo Gasparotto <gasp.eduardo@gmail.com>
2020-10-18 10:17:46 -0300

    Criado o projeto de console do TiaTurbina

---
+++ TiaTurbina.Apresentacao/TiaTurbina.Apresentacao.Console/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+using TiaTurbina.Bot;
+
+namespace TiaTurbina.Apresentacao.Console
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+           var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+
+
+            IServiceCollection services = new ServiceCollection();
+
+
+            services.AddSingleton<ServicoDeExecucaoDoBot>();
+            services.AddSingleton<IConfiguration>(configuration);
+            services.IniciarBot();
+
+            var serviceProvider = services.BuildServiceProvider();
+            var executor = serviceProvider.GetRequiredService<ServicoDeExecucaoDoBot>();
+
+            await executor.Iniciar();
+
+        }
+    }
+}
---
+++ TiaTurbina.Apresentacao/TiaTurbina.Apresentacao.Console/ServicoDeExecucaoDoBot.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+using TiaTurbina.Bot;
+
+namespace TiaTurbina.Apresentacao.Console
+{
+    class ServicoDeExecucaoDoBot
+    {
+        private ExecutorDoBot ExecutorDoBot { get; }
+
+        public ServicoDeExecucaoDoBot(ExecutorDoBot executorDoBot)
+        {
+            ExecutorDoBot = executorDoBot;
+        }
+
+        public async Task Iniciar()
+        {
+            await ExecutorDoBot.Executar();
+        }
+    }
+}
